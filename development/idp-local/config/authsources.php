074067925c885c28e3f0b11018eb68a6aef601c3
Matt Henderson <matt_henderson@sil.org>
2017-10-04 13:54:30 -0400

    Change mfa fields to be arrays of strings (like other fields).

--- development/idp-local/config/authsources.php
+++ development/idp-local/config/authsources.php
@@ -11,7 +11,7 @@ $config = [
             'mail' => ['no_mfa_needed@example.com'],
             'employeeNumber' => ['11111'],
             'cn' => ['NO_MFA_NEEDED'],
-            'promptForMfa' => 'no',
+            'promptForMfa' => ['no'],
             'mfaOptions' => [],
         ],
         'must_set_up_mfa:a' => [
@@ -22,7 +22,7 @@ $config = [
             'mail' => ['must_set_up_mfa@example.com'],
             'employeeNumber' => ['22222'],
             'cn' => ['MUST_SET_UP_MFA'],
-            'promptForMfa' => 'yes',
+            'promptForMfa' => ['yes'],
             'mfaOptions' => [],
         ],
         'has_backupcode:a' => [
@@ -33,14 +33,8 @@ $config = [
             'mail' => ['has_backupcode@example.com'],
             'employeeNumber' => ['33333'],
             'cn' => ['HAS_BACKUPCODE'],
-            'promptForMfa' => 'yes',
-            'mfaOptions' => [
-                [
-                    'id' => 37,
-                    'type' => 'backupcode',
-                    'data' => '',
-                ],
-            ],
+            'promptForMfa' => ['yes'],
+            'mfaOptions' => ['{"id":37, "type":"backupcode", "data":""}'],
         ],
         'has_totp:a' => [
             'eduPersonPrincipalName' => ['HAS_TOTP@mfa-idp.local'],
@@ -50,14 +44,8 @@ $config = [
             'mail' => ['has_totp@example.com'],
             'employeeNumber' => ['33333'],
             'cn' => ['HAS_TOTP'],
-            'promptForMfa' => 'yes',
-            'mfaOptions' => [
-                [
-                    'id' => 25,
-                    'type' => 'totp',
-                    'data' => '',
-                ],
-            ],
+            'promptForMfa' => ['yes'],
+            'mfaOptions' => ['{"id":25, "type":"totp", "data":""}'],
         ],
     ],
 ];
