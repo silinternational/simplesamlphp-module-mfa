297645e7d20ece9ab18f5ecf1e0703d5b9968252
Matt Henderson <matt_henderson@sil.org>
2017-10-24 16:51:58 -0400

    Fix missing/conflicting example user attributes.

--- development/idp-local/config/authsources.php
+++ development/idp-local/config/authsources.php
@@ -18,7 +18,13 @@ $config = [
             ],
         ],
         'must_set_up_mfa:a' => [
+            'eduPersonPrincipalName' => ['MUST_SET_UP_MFA@mfa-idp.local'],
+            'eduPersonTargetID' => ['22222222-2222-2222-2222-222222222222'],
+            'sn' => ['Set Up MFA'],
+            'givenName' => ['Must'],
+            'mail' => ['must_set_up_mfa@example.com'],
             'employeeNumber' => ['22222'],
+            'cn' => ['MUST_SET_UP_MFA'],
             'mfa' => [
                 'prompt' => 'yes',
                 'nag' => 'no',
@@ -26,12 +32,13 @@ $config = [
             ],
         ],
         'has_backupcode:a' => [
-            'eduPersonPrincipalName' => ['has_backupcode@mfa-idp.local'],
-            'eduPersonTargetID' => ['11111111-1111-1111-1111-111111111111'],
-            'sn' => ['Has'],
-            'givenName' => ['Backupcode'],
-            'mail' => ['hasBackupcode@example.com'],
+            'eduPersonPrincipalName' => ['HAS_BACKUPCODE@mfa-idp.local'],
+            'eduPersonTargetID' => ['33333333-3333-3333-3333-333333333333'],
+            'sn' => ['Backupcode'],
+            'givenName' => ['Has'],
+            'mail' => ['has_backupcode@example.com'],
             'employeeNumber' => ['33333'],
+            'cn' => ['HAS_BACKUPCODE'],
             'mfa' => [
                 'prompt' => 'yes',
                 'nag' => 'no',
@@ -45,7 +52,13 @@ $config = [
             ],
         ],
         'has_totp:a' => [
+            'eduPersonPrincipalName' => ['HAS_TOTP@mfa-idp.local'],
+            'eduPersonTargetID' => ['44444444-4444-4444-4444-444444444444'],
+            'sn' => ['TOTP'],
+            'givenName' => ['Has'],
+            'mail' => ['has_totp@example.com'],
             'employeeNumber' => ['33333'],
+            'cn' => ['HAS_TOTP'],
             'mfa' => [
                 'prompt' => 'yes',
                 'nag' => 'no',
@@ -59,7 +72,13 @@ $config = [
             ],
         ],
         'has_u2f:a' => [
+            'eduPersonPrincipalName' => ['HAS_U2F@mfa-idp.local'],
+            'eduPersonTargetID' => ['55555555-5555-5555-5555-555555555555'],
+            'sn' => ['U2F'],
+            'givenName' => ['Has'],
+            'mail' => ['has_u2f@example.com'],
             'employeeNumber' => ['55555'],
+            'cn' => ['HAS_U2F'],
             'mfa' => [
                 'prompt' => 'yes',
                 'nag' => 'no',
@@ -73,7 +92,13 @@ $config = [
             ],
         ],
         'nag_for_mfa:a' => [
-            'employeeNumber' => ['55555'],
+            'eduPersonPrincipalName' => ['NAG_FOR_MFA@mfa-idp.local'],
+            'eduPersonTargetID' => ['66666666-6666-6666-6666-666666666666'],
+            'sn' => ['For MFA'],
+            'givenName' => ['Nag'],
+            'mail' => ['nag_for_mfa@example.com'],
+            'employeeNumber' => ['666666'],
+            'cn' => ['NAG_FOR_MFA'],
             'mfa' => [
                 'prompt' => 'no',
                 'nag' => 'yes',
@@ -82,11 +107,12 @@ $config = [
         ],
         'has_all:a' => [
             'eduPersonPrincipalName' => ['has_all@mfa-idp.local'],
-            'eduPersonTargetID' => ['11111111-1111-1111-1111-111111111111'],
-            'sn' => ['Has'],
-            'givenName' => ['All'],
-            'mail' => ['has-all@example.com'],
-            'employeeNumber' => ['66666'],
+            'eduPersonTargetID' => ['77777777-7777-7777-7777-777777777777'],
+            'sn' => ['All'],
+            'givenName' => ['Has'],
+            'mail' => ['has_all@example.com'],
+            'employeeNumber' => ['777777'],
+            'cn' => ['HAS_ALL'],
             'mfa' => [
                 'prompt' => 'yes',
                 'nag' => 'no',
