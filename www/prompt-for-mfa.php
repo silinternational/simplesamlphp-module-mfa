58f062d9a217120887f140d3a5bea8a517befef3
Phillip Shipley <phillip.shipley@gmail.com>
2017-10-26 11:48:02 -0400

    remove mfa attributes before proceeding

--- www/prompt-for-mfa.php
+++ www/prompt-for-mfa.php
@@ -19,7 +19,7 @@ $cookieHash = filter_input(INPUT_COOKIE, 'c1') ?? ''; // hashed string
 $expireDate = filter_input(INPUT_COOKIE, 'c2') ?? 0;  // expiration timestamp
 if (Mfa::isRememberMeCookieValid(base64_decode($cookieHash), $expireDate, $mfaOptions, $state)) {
     // This condition should never return
-    // TODO should we strip MFA related attributes here?
+    unset($state['Attributes']['mfa']);
     SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
     throw new \Exception('Failed to resume processing auth proc chain.');
 }
