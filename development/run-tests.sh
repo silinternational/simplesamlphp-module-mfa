bfc15fe34ea63f7cf71a28b5c3a1cf89338beea8
Matt Henderson <matt_henderson@sil.org>
2021-06-14 17:45:24 -0400

    Stop trying to configure Logentries
    We no longer use that service.

--- development/run-tests.sh
+++ development/run-tests.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-runny ./setup-logentries.sh
-
 runny composer install --no-interaction --no-scripts --no-progress
 
 # Give composer time to install any new dependencies of this project
