ebb655f74bf5188016b24c766ceec489b41aaf44
Matt Henderson <matt_henderson@sil.org>
2021-06-15 17:34:52 -0400

    Show what commands are being run during tests

--- development/run-tests.sh
+++ development/run-tests.sh
@@ -3,6 +3,9 @@
 # Exit this script if a step fails
 set -e
 
+# Echo out the commands being run
+set -x
+
 composer install --no-interaction --no-scripts --no-progress
 
 # Give composer time to install any new dependencies of this project
