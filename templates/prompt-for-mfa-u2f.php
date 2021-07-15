f4dd5f0756d0f7910eefed898c0f8c2ce9b5d760
Matt Henderson <matt_henderson@sil.org>
2021-07-15 11:43:45 -0400

    Revert "Stop checking if browser supports u2f"
    This reverts commit 162ae6db128cc68e999ce720491793b124ad8d68.

--- templates/prompt-for-mfa-u2f.php
+++ templates/prompt-for-mfa-u2f.php
@@ -2,39 +2,49 @@
 $this->data['header'] = '2-Step Verification';
 $this->includeAtTemplateBase('includes/header.php');
 ?>
-<script src="<?=SimpleSAML\Module::getModuleURL('mfa/u2f-api.js');?>"></script>
-<script type="application/javascript">
-    window.onload = function() {
+<?php if ($this->data['supportsU2f']): ?>
+    <script src="<?=SimpleSAML\Module::getModuleURL('mfa/u2f-api.js');?>"></script>
+    <script type="application/javascript">
+        window.onload = function() {
 
-      console.log('data:');
-      console.log(<?=json_encode($this->data)?>);
+          console.log('data:');
+          console.log(<?=json_encode($this->data)?>);
 
-      var mfa = <?php echo json_encode($this->data['mfaOption']['data'], JSON_PRETTY_PRINT);?>;
-      console.log(mfa);
-      u2f.sign(mfa.appId, mfa.challenge, [mfa], function(response) {
-        console.log(response);
-        if (response.errorCode && response.errorCode != 0) {
-          alert("Error from U2F, code: " + response.errorCode);
-          return;
+          var mfa = <?php echo json_encode($this->data['mfaOption']['data'], JSON_PRETTY_PRINT);?>;
+          console.log(mfa);
+          u2f.sign(mfa.appId, mfa.challenge, [mfa], function(response) {
+            console.log(response);
+            if (response.errorCode && response.errorCode != 0) {
+              alert("Error from U2F, code: " + response.errorCode);
+              return;
+            }
+            var mfaForm = document.getElementById('mfaForm');
+            var mfaResponse = document.getElementById('mfaSubmission');
+            mfaResponse.value = JSON.stringify(response);
+            mfaForm.submit();
+          });
         }
-        var mfaForm = document.getElementById('mfaForm');
-        var mfaResponse = document.getElementById('mfaSubmission');
-        mfaResponse.value = JSON.stringify(response);
-        mfaForm.submit();
-      });
-    }
-</script>
+    </script>
+<?php endif; ?>
 <form method="post">
     <h2>USB Security Key</h2>
-    <p id="mfaInstructions">Please insert your security key and press its button.</p>
-    <p>
-        <input type="checkbox" name="rememberMe" id="rememberMe" value="true" checked="checked"/>
-        <label for="rememberMe">Remember this computer for 30 days</label>
-        <br />
-        <input type="hidden" id="mfaSubmission" name="mfaSubmission" />
-        <input type="hidden" id="submitMfa" name="submitMfa" />
-
-    </p>
+    <?php if ($this->data['supportsU2f']): ?>
+        <p id="mfaInstructions">Please insert your security key and press its button.</p>
+        <p>
+            <input type="checkbox" name="rememberMe" id="rememberMe" value="true" checked="checked"/>
+            <label for="rememberMe">Remember this computer for 30 days</label>
+            <br />
+            <input type="hidden" id="mfaSubmission" name="mfaSubmission" />
+            <input type="hidden" id="submitMfa" name="submitMfa" />
+            
+        </p>
+    <?php else: ?>
+        <p>
+            USB Security Keys are not supported in your current browser.
+            Please consider a more secure browser like
+            <a href="https://www.google.com/chrome/browser" target="_blank">Google Chrome</a>.
+        </p>
+    <?php endif; ?>
 
     <?php if (count($this->data['mfaOptions']) > 1): ?>
         <p>
